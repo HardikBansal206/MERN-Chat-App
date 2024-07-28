a9aed45a66257e78d9daf67a12f7affe362fe1c8
HardikBansal206 <hardik20062004@gmail.com>
2024-07-28 15:29:51 +0530

    minor fixes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
-const path = require("path");
 require("dotenv").config();
 
 app.use(cors());
@@ -26,27 +25,6 @@ mongoose
 app.get("/ping", (_req, res) => {
   return res.json({ msg: "Ping Successful" });
 });
-
-app.use(express.static(path.join(__dirname, "../public","build")));
-
-
-console.log( path.resolve(__dirname, "../public", "build", "index.html"))
-
-if (true) {
-  app.use(express.static(path.join(__dirname, "../public", "build", "index.html")));
-  
-  app.get("/api/healthcheck", function(req,res){
-    res.json({ status: 'ok' })
-  
-  })
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../public", "build", "index.html"))
-  );  
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
